4564a1aedf59a9404cbd12700a5ab48937b96a3a
Andreia Marujo <151871250+Andreia-Marujo@users.noreply.github.com>
2024-03-20 13:35:05 +0000

    Create helloWorld.py
    Hello World! Task 1

---
+++ helloWorld.py
@@ -0,0 +1,5 @@
+# This is a Python program that prints the message "Hello World!"
+# The next line is the print statement that displays the message
+
+# Print the message "Hello World!" to the console
+print("Hello World!")

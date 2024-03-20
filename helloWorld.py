51a92c796af1a382507d744b69964ac9132b721c
Andreia Marujo <151871250+Andreia-Marujo@users.noreply.github.com>
2024-03-20 14:14:29 +0000

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,5 @@
-# This is a Python program that prints the message "Git is Awesome!"
+# This is a Python program that prints the message "Hello World!"
 # The next line is the print statement that displays the message
 
-# Print the message "Git is Awesome!" to the console
-print("Git is Awesome!")
+# Print the message "Hello World!" to the console
+print("Hello World!")
